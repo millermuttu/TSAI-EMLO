d2ed8e26d88ec7f74dac475da41735ef77352091
millermuttu <flyingmuttus1995@gmail.com>
2022-02-20 15:45:39 +0530

    sagemaker, pytorch, and flask

--- microblog/microblog.py
+++ microblog/microblog.py
@@ -1,1 +1,6 @@
-from microblog.app import app
+from microblog.app import app, db
+from microblog.app.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
