90da565d7d9aecc1d580ca519814b08e892b79be
millermuttu <flyingmuttus1995@gmail.com>
2022-03-07 16:28:53 +0530

    microblog with docker

--- microblog/microblog.py
+++ microblog/microblog.py
@@ -1,5 +1,9 @@
-from microblog.app import app, db
-from microblog.app.models import User, Post
+from app import create_app, db, cli
+from app.models import User, Post
+
+app = create_app()
+cli.register(app)
+
 
 @app.shell_context_processor
 def make_shell_context():
